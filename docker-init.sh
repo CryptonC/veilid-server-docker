658dce8b692f80e4097e5346c6b5e682e28ef7f3
Crypton <crypton861@gmail.com>
2024-07-17 18:59:47 -0400

    Removed client listen port with the change of veilid control to ipc

--- docker-init.sh
+++ docker-init.sh
@@ -2,4 +2,4 @@
 
 apt update
 apt upgrade -y
-veilid-server -s logging.system.enabled=false -s logging.terminal.enabled=true -s client_api.listen_address=\"0.0.0.0:5959\" -f
+veilid-server -s logging.system.enabled=false -s logging.terminal.enabled=true -f
