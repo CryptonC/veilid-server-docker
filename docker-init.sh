427738bc42b3428d01eb523fc9cfec1a78bcf2de
Crypton <crypton861@gmail.com>
2025-05-17 11:30:57 -0400

    Removed automatic update from docker-init

--- docker-init.sh
+++ docker-init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
 publicIP=$(curl ipinfo.io 2>/dev/null | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 veilid-server -s logging.system.enabled=false -s logging.terminal.enabled=true -s core.network.protocol.udp.public_address=\"${publicIP}:5150\" -s core.network.protocol.tcp.public_address=\"${publicIP}:5150\"
